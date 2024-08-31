d77c7d4e27b7a6e08aa611ef26074171f8fa1b94
akashsathe <akashsathe063@gmail.com>
2024-08-31 13:21:22 +0530

    Implemetation of getCatergory api call respose is done

--- app/src/main/java/com/example/tweetsy/api/TweetsyApi.kt
+++ app/src/main/java/com/example/tweetsy/api/TweetsyApi.kt
@@ -13,9 +13,10 @@ import retrofit2.http.Headers
 interface TweetsyApi {
     //@Header is used to dynamic data
     @GET("/v3/b/66cef434e41b4d34e4266d54?meta=false")
-    suspend fun getTweets(@Header("X-JSON-Path")category: String):Response<List<TweetListItem>>
-//@Headers used to static data
+    suspend fun getTweets(@Header("X-JSON-Path") category: String): Response<List<TweetListItem>>
+
+    //@Headers used to static data
     @GET("/v3/b/66cef434e41b4d34e4266d54?meta=false")
     @Headers("X-JSON-Path:tweets..category")
-    suspend fun getCategories():Response<List<String>>
+    suspend fun getCategories(): Response<List<String>>
 }
